539d87e3e4986b7ebce464cc7a35a5d03b53d90e
Melissa Gray <mgray6200@gmail.com>
2025-02-28 21:47:27 -0500

    Update to build system
    Simplified `CMakeLists.txt` to use installed raylib library rather than
    downloading the repo and building every time.
    Updated `build.sh` to allow user to add arguments to specify a genertor or
    architecture. `build.sh` also compiles the executable automatically now.

--- build.sh
+++ build.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
-cmake -S . -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
+
+GEN="-G ${1}" # generator
+ARC="-A ${2}" # architecture
+
+if [ -z $1 ] # if $1 is empty, make GEN empty so CMake uses default generator
+then
+    echo "First parameter is empty"
+    GEN=""
+fi
+
+if [ -z $2 ] # if $2 is empty, make ARC empty so CMake uses default architecture
+then
+    echo "Second parameter is empty"
+    ARC=""
+fi
+
+
+# Use build system
+cmake -S . -B build $GEN $ARC -DCMAKE_BUILD_TYPE=Debug
+# Compile executable
+cmake --build build
